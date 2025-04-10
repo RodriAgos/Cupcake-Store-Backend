ab332960aea06555661abb115a00c0e0a7e2f023
Rodrigo A <rodrigo.aguirre88@gmail.com>
2025-04-10 12:53:33 -0400

    modificado script de añadido de productos (sin ID)

--- cupcakes.sql
+++ cupcakes.sql
@@ -1,11 +1,11 @@
-INSERT INTO cupcakes (cupcake_id, name, description, price, image_url, stock) VALUES
-(1, 'Choco classic', 'Una bomba con un bizcocho de chocolate...', 2990, '/images/chococlassicA.png', 20),
-(2, 'Tuki cupcake', 'Lo más trasandino que ofrecemos...', 2990, '/images/tuki.png', 20),
-(3, 'Pie de Limón', 'Sabor clásico y delicioso...', 2990, '/images/limonA.png', 20),
-(4, 'Savage mint', 'Una experiencia para los fanáticos de la menta.', 2990, '/images/MintCupcakeA.png', 20),
-(5, 'Nutella cupcake', 'Bizcocho de vainilla relleno de Nutella.', 2990, '/images/NutelloA.png', 20),
-(6, 'Strawberry cupcake Forever', 'Con crema de frambuesa y frutilla fresca.', 2990, '/images/strawberryA.png', 20),
-(7, 'Kuky Kupcake', 'Decorado con una galleta completa.', 2990, '/images/KukyCupcakeA.png', 20),
-(8, 'Baileys cupcake', 'Con shot de crema de Baileys.', 2990, '/images/BaileysCupcakeA.png', 20),
-(9, 'Donkey Kong Kupcake', 'Decorado con plátano confitado.', 2990, '/images/DonkeyCupCake.png', 20),
-(10, 'Tritón cupcake', 'Decorado con galleta triturada.', 2990, '/images/TritonCupcakeA.png', 20);
+INSERT INTO cupcakes (name, description, price, image_url, stock) VALUES
+('Choco classic', 'Una bomba con un bizcocho de chocolate...', 2990, '/images/chococlassicA.png', 20),
+('Tuki cupcake', 'Lo más trasandino que ofrecemos...', 2990, '/images/tuki.png', 20),
+('Pie de Limón', 'Sabor clásico y delicioso...', 2990, '/images/limonA.png', 20),
+('Savage mint', 'Una experiencia para los fanáticos de la menta.', 2990, '/images/MintCupcakeA.png', 20),
+('Nutella cupcake', 'Bizcocho de vainilla relleno de Nutella.', 2990, '/images/NutelloA.png', 20),
+('Strawberry cupcake Forever', 'Con crema de frambuesa y frutilla fresca.', 2990, '/images/strawberryA.png', 20),
+('Kuky Kupcake', 'Decorado con una galleta completa.', 2990, '/images/KukyCupcakeA.png', 20),
+('Baileys cupcake', 'Con shot de crema de Baileys.', 2990, '/images/BaileysCupcakeA.png', 20),
+('Donkey Kong Kupcake', 'Decorado con plátano confitado.', 2990, '/images/DonkeyCupCake.png', 20),
+('Tritón cupcake', 'Decorado con galleta triturada.', 2990, '/images/TritonCupcakeA.png', 20);
