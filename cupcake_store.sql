30d0f0f522425153a6d85d9f56b107aad67ffcc8
MadMark <mqmadrid@gmail.com>
2025-04-12 01:25:18 -0400

    sql

---
+++ cupcake_store.sql
@@ -0,0 +1,14 @@
+-- Database: cupcake_store
+
+-- DROP DATABASE IF EXISTS cupcake_store;
+
+CREATE DATABASE cupcake_store
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Chile.1252'
+    LC_CTYPE = 'Spanish_Chile.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
