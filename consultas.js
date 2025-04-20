34bf412895f02896cb46bd2e8370165610e46d97
Javier Garin <dev.javiergarin@gmail.com>
2025-04-20 18:13:10 -0400

    update files

--- consultas.js
+++
@@ -1,2 +0,0 @@
-// db.js contiene las consultas en mi rama, cucho.
-
--- index.js
+++ index.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
-const productRoutes = require('./routes/productRoutes'); // 👈 nuevo
-const cartRoutes = require('./routes/cartRoutes'); // 👈 nuevo
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes'); 
 const orderRoutes = require('./routes/orders');
-
 
 
 const app = express();
@@ -16,8 +15,8 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes); // 👈 nuevo
-app.use('/api/cart', cartRoutes); // 👈 nuevo
+app.use('/api/products', productRoutes); 
+app.use('/api/cart', cartRoutes); 
 app.use('/api/orders', orderRoutes);
 
 
